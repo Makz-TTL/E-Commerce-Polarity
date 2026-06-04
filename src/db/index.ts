306e7e037848b826b96bee856882f4d82cd04cf3
Maxim <maxmatto13@gmail.com>
2026-06-04 09:10:28 +0200

    relations fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3"
-import * as schema from "./schema"
-import { relations } from "./schema"
+import  { relations } from "./schema/relations"
+import { schema } from "./schema/schema"
 import path from "node:path"
 
 const dbPath = path.join(process.cwd(), "sqlite.db")
--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -3,49 +3,35 @@ import { products } from "./products"
 import { reviews } from "./reviews"
 import { productUsefulness } from "./productUsefulness"
 import {orders} from "./orders"
-import { relations } from "drizzle-orm/relations";
+import { defineRelations } from "drizzle-orm/relations";
+
+export default defineRelations({ users, products, reviews, productUsefulness, orders }, (r) => ({
+  users: {
+    reviews: r.many.reviews(),
+    orders: r.many.orders()
+  },
+  products: {
+    reviews: r.many.reviews(),
+    productUsefulness: r.many.productUsefulness()
+  },
+  reviews: {
+    user: r.one.users({
+      from: r.reviews.userId,
+      to: r.users.id
+    }),
+    product: r.one.products({
+      from: r.reviews.productId,
+      to: r.products.id
+    }),
+ 
+  },
+  orders: {
+    user: r.one.users({
+      from: r.orders.userId,
+      to: r.users.id
+    })
+  }
+}))
 
 
 
-// Relazioni per gli UTENTI
-export const usersRelations = relations(users, ({ many }) => ({
-  orders: many(orders),
-  reviews: many(reviews),
-  usefulnessVotes: many(productUsefulness),
-  soldProducts: many(orders)
-}));
-
-// Relazioni per i PRODOTTI
-export const productsRelations = relations(products, ({ one, many }) => ({
-  reviews: many(reviews),
-  seller: one(users, {
-    fields: [products.userId],
-    references: [users.id],
-  }),
-}));
-
-// Relazioni per le RECENSIONI
-export const reviewsRelations = relations(reviews, ({ one, many }) => ({
-  author: one(users, {
-    fields: [reviews.userId],
-    references: [users.id],
-  }),
-  product: one(products, {
-    fields: [reviews.productId],
-    references: [products.id],
-  }),
-  usefulness: many(productUsefulness),
-}));
-
-
-// Relazioni per gli ORDINI
-export const ordersRelations = relations(orders, ({ one, many }) => ({
-  buyer: one(users, {         //Relazione tra ORDINI e UTENTI (buyer).
-    fields: [orders.userId],  //Un ordine ha un solo buyer.
-    references: [users.id],
-  }),
-  product: many(products, {        //Relazione tra ORDINI e PRODOTTI.
-    fields: [orders.productId],     //Un ordine può contenere più prodotti.
-    references: [products.id],
-  }),
-}));
