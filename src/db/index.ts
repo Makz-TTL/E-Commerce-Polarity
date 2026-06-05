9175b8e7e7768ef6128c0d9c111771e2ba416811
cristianimpossibile07 <cristianimpossibile@gmail.com>
2026-06-05 09:32:42 +0200

    postgress

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,20 @@
-import { drizzle } from "drizzle-orm/libsql"
-import { createClient } from "@libsql/client"
+import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "./schema"
 import relations from "./schema/relations"
 import path from "node:path"
+import Postgres from 'postgres'
 
-const dbPath = "file:" + path.join(process.cwd(), "local.db")
-const client = createClient({ url: dbPath })
+const postgres = Postgres({
+    "host": "",
+    user: "",
+    password: "",
+    database: "",
+    port: 0
+})
 
-export const db = drizzle({ client, schema, relations })
+
+export const db = drizzle({
+    client: postgres,
+    schema,
+    relations,
+})
