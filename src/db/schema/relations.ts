98018fbf414e6ce8950fa362cbd0fc02736a01f6
Maxim <maxmatto13@gmail.com>
2026-06-05 09:27:42 +0200

    relations fix push

--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -1,27 +1,30 @@
-
 import { users } from "./users"
 import { products } from "./products"
 import { reviews } from "./reviews"
 import { productUsefulness } from "./productUsefulness"
-import {orders} from "./orders"
+import { orders } from "./orders"
+import { wishlist } from "./wishlist"
 import { defineRelations } from "drizzle-orm/relations";
-import {wishlist} from "./wishlist"
 
 export default defineRelations({ users, products, reviews, productUsefulness, orders, wishlist }, (r) => ({
   users: {
     reviews: r.many.reviews(),
     orders: r.many.orders(),
-    wishlist: r.many.wishlist()
-
+    wishlist: r.many.wishlist(),
+    productUsefulness: r.many.productUsefulness(),
   },
+  
   products: {
+   
     seller: r.one.users({
-      from: r.products.userId,
+      from: r.products.userId, 
       to: r.users.id,
     }),
     reviews: r.many.reviews(),
     productUsefulness: r.many.productUsefulness(),
+    orders: r.many.orders(), 
   },
+
   reviews: {
     user: r.one.users({
       from: r.reviews.userId,
@@ -31,8 +34,8 @@ export default defineRelations({ users, products, reviews, productUsefulness, or
       from: r.reviews.productId,
       to: r.products.id
     }),
- 
   },
+
   productUsefulness: {
     product: r.one.products({
       from: r.productUsefulness.productId,
@@ -43,6 +46,7 @@ export default defineRelations({ users, products, reviews, productUsefulness, or
       to: r.users.id,
     }),
   },
+
   orders: {
     user: r.one.users({
       from: r.orders.userId,
@@ -53,7 +57,16 @@ export default defineRelations({ users, products, reviews, productUsefulness, or
       to: r.products.id,
     }),
   },
+
+
+  wishlist: {
+    user: r.one.users({
+      from: r.wishlist.userId,
+      to: r.users.id,
+    }),
+    product: r.one.products({
+      from: r.wishlist.productId,
+      to: r.products.id,
+    }),
+  },
 }))
-
-
-
