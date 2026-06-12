8ed033d8f10a9ce705c0c5b0956b4d951d0d7ccd
Maxim <maxmatto13@gmail.com>
2026-06-12 12:05:50 +0200

    code optimization

--- src/client/components/ProfilePage.tsx
+++ src/client/components/ProfilePage.tsx
@@ -94,11 +94,14 @@ export default async function PorfilePage({ username }: Props) {
                             }
                         }
 
-                        // Gestione dinamica dello stato (Approvato, In attesa, Rifiutato)
+                        // Gestione dinamica dello stato (Approvato, In revisione, In attesa, Rifiutato)
                         let statusColorClass = "bg-amber-400 ring-amber-50"
                         let statusText = "In attesa"
 
-                        if (product.status === "approved") {
+                        if (product.status === "reviewing") {
+                            statusColorClass = "bg-blue-500 ring-blue-50 animate-pulse"
+                            statusText = "In revisione"
+                        } else if (product.status === "approved") {
                             statusColorClass = "bg-emerald-500 ring-emerald-50"
                             statusText = "Approvato"
                         } else if (product.status === "rejected") {
